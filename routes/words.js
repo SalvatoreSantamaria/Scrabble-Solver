7c2ba39320d37e065693504c08c2278cdf79d2e1
Salvatore Santamaria <sam.santamaria@gmail.com>
2020-10-07 21:07:44 -0700

    Split and return valid Scrabble words now working

--- routes/words.js
+++ routes/words.js
@@ -26,9 +26,42 @@ router.get('/', (req, res) => {
   // ], 'ha');
   // console.log(result)
 
+  let valid_scrabble_words = []
+  // findAnagrams will only return anagrams
+  // result.push(findAnagrams(all_words, 'ha'))
+  // console.log(result)
 
-  let result = findAnagrams(all_words, 'ha')
-  console.log(result)
+  //works
+  //console.log(all_words.includes('a'))
+
+
+  //works, splits up a words into all possible combinations
+  var tree = function(leafs) {
+    var branches = [];
+    if (leafs.length == 1) return leafs;
+    for (var k in leafs) {
+      var leaf = leafs[k];
+      tree(leafs.join('').replace(leaf, '').split('')).concat("").map(function(subtree) {
+        branches.push([leaf].concat(subtree));
+      });
+    }
+    return branches;
+  };
+  let letterCombinations = tree("hat".split('')).map(function(str) {
+    return str.join('')
+  })
+  //console.log(letters)
+
+  // works, checks to see if the letterCombos are valid against the all_words
+  //let letterCombiations = ["hat","ah","ha","th","at","a",'h','t','ht','ta','tha']
+  letterCombinations.forEach(letterCombo => {
+    if (all_words.includes(letterCombo)) {
+      valid_scrabble_words.push(letterCombo)
+    }
+  });
+
+  console.log(valid_scrabble_words)
+
 
   res.send('Hello world')
 })
